9aa0f01988416f9ca6a64fb8bd98848a453492fc
Adam Nash <adam.j.nash@gmail.com>
2026-05-26 12:17:45 -0400

    Fix duplicated lands in draft structure

--- scripts/print_draft_file.py
+++ scripts/print_draft_file.py
@@ -48,8 +48,9 @@ def generateFile(code):
 			slot_name = slot['name']
 			if slot_name in [ 'wildcard', 'foil' ] and not filtered(card, filters) and not 'Basic' in card['type'] and not 'token' in card['shape']:
 				booster[slot_name].append(card)
-			elif slot_name == 'nonbasic land' and not 'Basic' in card['type'] and 'Land' in card['type'] and not 'token' in card['shape']:
-				booster[slot_name].append(card)
+			elif not 'Basic' in card['type'] and 'Land' in card['type'] and not 'token' in card['shape']:
+				if slot_name == 'nonbasic land':
+					booster[slot_name].append(card)
 			elif not slot['custom']:
 				if ((card['rarity'] in ['common', 'masterpiece'] and slot_name == 'common | masterpiece') or (card['rarity'] == 'mythic' and slot_name == 'rare') or card['rarity'] == slot_name) and not filtered(card, filters) and not 'Basic' in card['type'] and not 'token' in card['shape']:
 					booster[slot_name].append(card)
